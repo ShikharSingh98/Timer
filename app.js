b77b856b6b7f19e04ca3f1c09238910d2fead4c7
ShikharSingh98 <shikharsingh98@gmail.com>
2022-06-18 10:38:48 +0530

    added logic for updating border

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-
-  start = () => {
-    this.tick();
-    this.intervalId = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.intervalId);
-  };
-
-  tick = () => {
-    console.log('tick');
-  };
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('Timer Started');
+  },
+  onTick() {
+    console.log('Timer just ticked down');
+  },
+  onComplete() {
+    console.log('Timer completed');
+  },
+});
